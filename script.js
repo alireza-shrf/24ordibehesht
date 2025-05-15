210aa9b1680512eee0f6dd665b2d372d49c2c9cc
alireza-shrf <sharafialireza180@gmail.com>
2025-05-15 15:01:31 +0330

    1

---
+++ script.js
@@ -0,0 +1,5 @@
+let menueitem=["home","products","contactus","aboutus"];
+document.getElementById("home").innerText=menueitem[0];
+document.getElementById("pro").innerText=menueitem[1];
+document.getElementById("contact").innerText=menueitem[2];
+document.getElementById("about").innerText=menueitem[menueitem.length-1];
